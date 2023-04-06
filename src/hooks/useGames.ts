aee53109e0b55e943b8c98414d883e75ab2dab81
ghaith sheibani <ghaithshpro@gmail.com>
2023-04-06 20:55:19 +0300

    Create a custom hook to fetch the game

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { Text } from "@chakra-ui/react";
+import { CanceledError } from "axios";
+interface Game {
+  id: number;
+  name: string;
+}
+interface FetchGamesResponse {
+  count: string;
+  results: Game[];
+}
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("./games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+  }, []);
+  return { games, error };
+};
+
+export default useGames;
